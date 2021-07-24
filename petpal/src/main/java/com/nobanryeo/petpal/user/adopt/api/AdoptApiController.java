27ec6fe94dffa3ae5e53a8f48475917b9ebb6a7f
juhee <fhqj21@gmail.com>
2021-07-25 02:39:36 +0900

    Add Swagger - 210725

---
+++ petpal/src/main/java/com/nobanryeo/petpal/user/adopt/api/AdoptApiController.java
@@ -0,0 +1,11 @@
+package com.nobanryeo.petpal.user.adopt.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api()
+public class AdoptApiController {
+
+}
