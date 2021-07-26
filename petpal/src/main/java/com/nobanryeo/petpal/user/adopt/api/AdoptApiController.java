74a90c4d1272a0102a60488c54ac2fd82015d8d0
juhee <fhqj21@gmail.com>
2021-07-27 02:10:11 +0900

    api swagger-ui success_210727

--- petpal/src/main/java/com/nobanryeo/petpal/user/adopt/api/AdoptApiController.java
+++ petpal/src/main/java/com/nobanryeo/petpal/user/adopt/api/AdoptApiController.java
@@ -1,11 +1,65 @@
 package com.nobanryeo.petpal.user.adopt.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.nobanryeo.petpal.user.adopt.service.AdoptService;
+import com.nobanryeo.petpal.user.dto.AdoptPictureManageDTO;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api()
+@Api("Adopt Controller API V1")
+@RequestMapping("/user/*")
 public class AdoptApiController {
 
+
+	private final AdoptService adoptService;
+	
+	@Autowired
+	public AdoptApiController(AdoptService adoptService) {
+		this.adoptService = adoptService;
+	} 
+	
+	@GetMapping("adopt")
+	public String introAdopt() {
+
+		return "user/adopt/adoptPage";
+	}
+	
+	
+	@ApiOperation("adopt 전체 리스트 조회")
+	@GetMapping("adoptData")
+	public ModelAndView selectAdoptMain(ModelAndView mv, HttpServletResponse response) {
+			
+			response.setContentType("application/json; charset=utf-8");
+			List<AdoptPictureManageDTO> adoptList = new ArrayList<>();
+			adoptList=adoptService.selectAdoptList();
+			
+	//		System.out.println("adoptList in controller: "+adoptService.selectAdoptList());
+			System.out.println("controller: "+adoptList);
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting()
+					.setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
+					.serializeNulls().disableHtmlEscaping().create();
+		
+			mv.addObject("adoptList", gson.toJson(adoptList));
+			mv.setViewName("jsonView");
+			
+			return mv;
+		}
+	
+	
 }
--- petpal/src/main/java/com/nobanryeo/petpal/user/adopt/common/SwaggerConfiguration.java
+++ petpal/src/main/java/com/nobanryeo/petpal/user/adopt/common/SwaggerConfiguration.java
@@ -2,24 +2,46 @@ package com.nobanryeo.petpal.user.adopt.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.google.common.base.Predicates;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+@EnableWebMvc
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(Predicates.not(RequestHandlerSelectors.basePackage("com.nobanryeo.petpal.user.adopt.api")))
-				.paths(PathSelectors.any()).build();
+				.apis(RequestHandlerSelectors.basePackage("com.nobanryeo.petpal.user.adopt.api"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(apiInfo());
 		
+	}
+	
+	//스프링 부트 없이 설정하기 위해 수동설정
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+		        .addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**")
+		        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Adopt Board Api")
+				                    .description("PETPAL project main application Adopt")
+				                    .version("1.0")
+				                    .license("Copyright Juhee Hwang")
+				                    .build();
 	}
 }
